90030e5cd62bef6d9bdc9c9145bc8613923f544d
Kyle Tower <kyle.tower@gmail.com>
2021-07-23 11:56:07 -0400

    refactoring SongQueue

--- src/SongQueue.js
+++ src/SongQueue.js
@@ -3,6 +3,7 @@
 // and/or pass the Search Results as a prop.
 import { sortByTime, sortByVotes } from './App';
 import { userId } from './App';
+import { useState } from 'react';
 
 const SongQueue = ({
   sortOrder,
@@ -25,9 +26,12 @@ const SongQueue = ({
       return b.id - a.id;
     });
   }
-  let myRequests = [];
+
+  // let [songsToDisplay, setSongsToDisplay] = useState(queue);
+  let songsToDisplay = queue;
   if (onlyMyRequests) {
-    myRequests = queue.filter((request) => request.userId === userId);
+    // setSongsToDisplay(queue.filter((request) => request.userId === userId));
+    songsToDisplay = queue.filter((request) => request.userId === userId);
   }
 
   return (
@@ -43,74 +47,51 @@ const SongQueue = ({
       >
         Sort By Likes/Time Added
       </button>
-      {!onlyMyRequests &&
-        queue.map((song) => (
-          <div className='queue-item' key={song.id}>
-            <div className='album-art'>
-              <img
-                src={
-                  song.albumArt ||
-                  'https://www.last.fm/static/images/defaults/player_default_album.430223706b14.png'
-                }
-                width='34'
-                alt=''
-              />
-            </div>
-            <div className='song-info'>
-              <p className='song-name'>{song.songTitle}</p>
-              <p className='artist-name'>{song.artistName}</p>
-            </div>
-            <div className='vote'>
-              <button onClick={() => upVote(song)}>
-                {song.upVotes}
-                <span className='emoji'>👍</span>
-              </button>
-              <button onClick={() => markAsPlayed(song.id)}>
-                <span className='emoji'>▶️</span>
-              </button>
-              <button onClick={() => downVote(song)}>
-                {song.downVotes}
-                <span className='emoji'>👎</span>
-              </button>
-            </div>
-          </div>
-        ))}
 
-      {onlyMyRequests &&
-        myRequests.map((song) => (
-          <div className='queue-item' key={song.id}>
-            <div className='album-art'>
-              <img
-                src={
-                  song.albumArt ||
-                  'https://www.last.fm/static/images/defaults/player_default_album.430223706b14.png'
-                }
-                width='34'
-                alt=''
-              />
-            </div>
-            <div className='song-info'>
-              <p className='song-name'>{song.songTitle}</p>
-              <p className='artist-name'>{song.artistName}</p>
-            </div>
-            <div className='vote'>
-              <button onClick={() => upVote(song)}>
-                {song.upVotes}
-                <span className='emoji'>👍</span>
-              </button>
-              <button onClick={() => downVote(song)}>
-                {song.downVotes}
-                <span className='emoji'>👎</span>
-              </button>
-              <button onClick={() => markAsPlayed(song.id)}>
-                <span className='emoji'>▶️</span>
-              </button>
-            </div>
-          </div>
-        ))}
-      {/* </div> */}
+      <DisplaySongs
+        songsToDisplay={songsToDisplay}
+        upVote={upVote}
+        downVote={downVote}
+        markAsPlayed={markAsPlayed}
+      />
     </>
   );
 };
 
 export default SongQueue;
+
+const DisplaySongs = ({ songsToDisplay, upVote, downVote, markAsPlayed }) => {
+  return songsToDisplay.map((song) => {
+    return (
+      <div className='queue-item' key={song.id}>
+        <div className='album-art'>
+          <img
+            src={
+              song.albumArt ||
+              'https://www.last.fm/static/images/defaults/player_default_album.430223706b14.png'
+            }
+            width='34'
+            alt=''
+          />
+        </div>
+        <div className='song-info'>
+          <p className='song-name'>{song.songTitle}</p>
+          <p className='artist-name'>{song.artistName}</p>
+        </div>
+        <div className='vote'>
+          <button onClick={() => upVote(song)}>
+            {song.upVotes}
+            <span className='emoji'>👍</span>
+          </button>
+          <button onClick={() => markAsPlayed(song.id)}>
+            <span className='emoji'>▶️</span>
+          </button>
+          <button onClick={() => downVote(song)}>
+            {song.downVotes}
+            <span className='emoji'>👎</span>
+          </button>
+        </div>
+      </div>
+    );
+  });
+};
